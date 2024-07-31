20318ffb43157f063b19db3f76ccd3f3ec709b70
Aditya Patel <aditya.patel@nucleusteq.com>
2024-07-31 17:52:49 +0530

    updating DTO

---
+++ BankingSystem/src/main/java/com/banking/DTO/CustomerDto.java
@@ -0,0 +1,31 @@
+package com.banking.DTO;
+
+public class CustomerDto {
+    String firstName;
+    String lastName;
+    String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

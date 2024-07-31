d35ef959387350f2a49d6e7d43059ecc77ffe6dd
Aditya Patel <aditya.patel@nucleusteq.com>
2024-07-31 15:13:09 +0530

    updated banking system with entities

--- MultiThreadExercise/src/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-public class ExceptionHandler {
-    public static class InsufficientBalanceException extends Exception {
-        public InsufficientBalanceException(){
-            super("Insufficient Balance Exception!");
-        }
-    }
-}

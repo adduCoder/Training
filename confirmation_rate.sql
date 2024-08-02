148c6970bc27651f176ad6f53e3c8ac335f42f57
Aditya Patel <aditya.patel@nucleusteq.com>
2024-08-02 22:03:24 +0530

    join and aggregate function with condition

---
+++ confirmation_rate.sql
@@ -0,0 +1,33 @@
+/*
+1934. Confirmation Rate
+
+Table: Signups
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
++----------------+----------+
+user_id is the column of unique values for this table.
+Each row contains information about the signup time for the user with ID user_id.
+ 
+
+Table: Confirmations
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
+| action         | ENUM     |
++----------------+----------+
+(user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+user_id is a foreign key (reference column) to the Signups table.
+action is an ENUM (category) of the type ('confirmed', 'timeout')
+Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+*/
+# Write your MySQL query statement below
+select s.user_id , round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate 
+from signups s left join confirmations c on s.user_id=c.user_id
+group by s.user_id;
